18a7de11b7e7797aa5a0f55cb44490c111ca5238
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-12-28 21:00:36 +0530

    modifing import

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <math.h>
+#include <glm/gtc/type_ptr.hpp>
 #include <stdbool.h>
 #include <stdio.h>
 ///
